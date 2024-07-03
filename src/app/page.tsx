63556299f511cf114bea0bbdcf0dce8b9bac497d
cmg8431 <cmg8431@gmail.com>
2024-07-03 19:30:32 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
-import { stackRouterBack, stackRouterPush } from "@/utils/stackReouter";
-import { useRouter } from "next/navigation";
+import { stackRouterPush } from "@/utils/stackReouter";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <main className={styles.main}>
-      <button onClick={() => stackRouterPush(router, "/detail")}>
-        {"push 이동"}
-      </button>
+      <button onClick={() => stackRouterPush("/detail")}>{"push 이동"}</button>
     </main>
   );
 }
